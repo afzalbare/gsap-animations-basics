a4e5fe54cf0a219caf3f9833ce1da388ef3f9c32
afzalbare <afzalbare26@gmail.com>
2026-04-23 17:17:46 +0600

    box animation end

--- gsap-1/main.js
+++ gsap-1/main.js
@@ -9,3 +9,13 @@ gsap.from("#box2",{
     duration: 2, 
     delay: 1,  
 })
+
+gsap.to("#box3",{
+    x: 900,
+    duration: 3, 
+    delay: 1.5,  
+    rotate: 360,
+    backgroundColor: "green",
+    borderRadius: "50%",
+    scale: 0.5,
+})
