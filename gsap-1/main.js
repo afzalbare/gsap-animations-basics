e44821bc62fb1a5e600ad526fba2618b245a9dce
afzalbare <afzalbare26@gmail.com>
2026-04-25 15:35:03 +0600

    yoyo add

--- gsap-1/main.js
+++ gsap-1/main.js
@@ -20,4 +20,25 @@ gsap.to("#box3",{
     scale: 0.5,
 })
 
+gsap.to(".box__1",{
+    x: 900,
+    duration: 1, 
+    delay: 1,  
+    rotate: 360,
+    backgroundColor: "green",
+    borderRadius: "50%",
+    scale: 0.5,
+    yoyo: true,
+    repeat: -1,
+})
+
 // End Box Animation 
+
+gsap.from("h2",{
+    opacity:0,
+    duration: 1,
+    y:30,
+    delay: 1,
+    stagger:1,
+})
+
