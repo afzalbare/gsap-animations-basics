0430ab1bfddec4c63b571a1853736f80d6095488
afzalbare <afzalbare26@gmail.com>
2026-05-04 11:58:42 +0600

    part-2

--- gsap-2/main.js
+++ gsap-2/main.js
@@ -1,21 +1,45 @@
 gsap.from("#page-1 #box-1", {
-    scale: 0,
-    duration: 1,
-    delay: 1,
-    rotate: 360,
+  scale: 0,
+  duration: 1,
+  delay: 1,
+  rotate: 360,
 });
 
 gsap.from("#page-2 #box-2", {
-    scale: 0,
-    duration: 1,
-    delay: 1,
-    rotate: 360,
-    scrollTrigger: {
-        trigger: "#page-2 #box-2",
-        scroller:"body",
-        markers: true,
-        start: "top 50%",
-    },
+  scale: 0,
+  duration: 1,
+  delay: 1,
+  rotate: 360,
+  scrollTrigger: {
+    trigger: "#page-2 #box-2",
+    scroller: "body",
+    markers: true,
+    start: "top 50%",
+  },
+});
+
+gsap.from("#page-2 h2", {
+  opacity: 0,
+  duration: 2,
+  x: 500,
+  scrollTrigger: {
+    trigger: "#page-2, h2",
+    scroller: "body",
+    markers: true,
+    start: "top 50%",
+  },
+});
+
+gsap.from("#page-2 h3", {
+  opacity: 0,
+  duration: 2,
+  x: -500,
+  scrollTrigger: {
+    trigger: "#page-2, h2",
+    scroller: "body",
+    markers: true,
+    start: "top 50%",
+  },
 });
 
 // gsap.from("#page-3 #box-3", {});
